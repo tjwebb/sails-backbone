03964d3b256821fde763b1e1826cc2b2661c0e1a
Travis Webb <me@traviswebb.com>
2014-08-12 03:08:15 -0400

    separated generator and parser into separate modules

---
+++ generator.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var _ = require('lodash');
+var rules = _.keys(require('anchor/lib/match/rules'));
+var path = require('path');
+
+/**
+ * Generate Backbone Models and Collections objects from the Sails.js API.
+ * @param sails
+ * @param pkg
+ */
+module.exports = function (sails, pkg) {
+  var domain = _.intersection(_.keys(sails.models), _.keys(sails.controllers));
+
+  return {
+    models: _.map(domain, function (name) {
+      return createModel(sails, pkg, name);
+    }),
+    version: pkg.version
+  };
+};
+
+function createModel (sails, pkg, name) {
+  var model = sails.models[name];
+  var globalId = model.globalId;
+
+  return {
+    name: globalId,
+    idAttribute: getPrimaryKey(model),
+    urlRoot: path.join(sails.config.blueprints.prefix, name),
+    defaults: getDefaults(model),
+    relations: getRelations(sails, model),
+    validations: getValidations(sails, model)
+  };
+}
+
+function getPrimaryKey (model) {
+  var pk = _.find(_.keys(model.definition), function (key) {
+    return model.definition[key].primaryKey === true;
+  });
+  return pk || 'id';
+}
+
+function getRelations (sails, model) {
+  var relationAttributes = _.filter(_.keys(model.definition), function (key) {
+    var attribute = model.attributes[key];
+    return attribute.model || attribute.collection;
+  });
+  return _.map(relationAttributes, function (key) {
+    var relation = model.definition[key];
+
+    return {
+      type: relation.model ? 'HasOne' : 'HasMany',
+      key: key,
+      relatedModel: sails.models[relation.model || relation.collection].globalId,
+      reverseRelation: relation.via ? {
+        key: relation.via
+      } : undefined
+    };
+
+  });
+}
+
+function getValidations (sails, model) {
+  return _.object(
+    _.keys(model.definition),
+    _.map(model.definition, function (attribute, key) {
+      return _.compact(_.intersection(_.keys(attribute), rules).concat(attribute.type));
+    })
+  );
+}
+
+function getDefaults (model) {
+  return _.object(
+    _.filter(_.keys(model.definition), _.partial(_.has, 'defaultsTo')),
+    _.pluck(_.values(model.definition), 'defaultsTo')
+  );
+}
